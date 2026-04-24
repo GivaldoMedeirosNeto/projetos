aea4767ec219592ae09af3a9a9333008d12c899b
Givaldo Medeiros Neto <givaldo@gsmn.dev.br>
2026-04-24 14:03:14 -0300

    Classe de Conexão com o banco

---
+++ Intranet/ibisnatal/src/main/java/connection/SingleConnection.java
@@ -0,0 +1,44 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SingleConnection {
+	
+	private static String url = "jdbc:mysql://localhost:3306/ibisnatal?noAccessToProcedureBodies=true";
+	private static String user = "ibisnat";
+	private static String pass = "T9@vL#7qZ!mP4$Ks";
+	private static Connection connection = null;
+	
+	static {
+		conectar();
+	}
+	
+	public SingleConnection() {
+		conectar();
+	}
+	
+	private static void conectar() {
+		
+		try {
+			
+			if(connection == null) {
+				
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				connection = DriverManager.getConnection(url, user, pass);
+				connection.setAutoCommit(false);
+				System.out.println("conexão realizada.");
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static Connection getConnection() {
+		return connection;
+	}
+
+}
